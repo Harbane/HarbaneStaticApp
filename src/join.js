5b95fa224a40295d657567aea97844d3d935a6d1
Harbane <haroonahmad@outlook.com>
2023-07-29 21:12:50 +1000

    input focus color

---
+++ src/join.js
@@ -0,0 +1,15 @@
+
+
+function submit30() {
+  let fname = document.getElementById('fname').value;
+  let lname = document.getElementById('lname').value;
+  let email = document.getElementById('email').value;
+
+  console.log('sending user registration');
+  console.log(`First Name: ${fname}`);
+  console.log(`Last Name: ${lname}`);
+  console.log(`Email: ${email}`);
+
+}
+
+
